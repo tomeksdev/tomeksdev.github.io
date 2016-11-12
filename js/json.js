d1994956c0f2cda1b382af496763e16baddf6752
Tomo Novak <tomeknomek@gmail.com>
2016-11-12 13:36:44 +0100

    Website v.1.1.6
    Update json file for blog post.

--- js/json.js
+++ js/json.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var url = "https://tomeksdev.github.io/post/posts.json" ; 
+    var url = "https://tomeksdev.github.io/post/post.json" ; 
     $.getJSON( url, function( json ) {
         console.log(json);
         $("#post-title").html(json.posts.post[0].title);
