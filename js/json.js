ff46762dd8acb210437b2eeeaa86bd123ffeb6f1
Tomo Novak <tomeknomek@gmail.com>
2016-11-12 15:46:05 +0100

    project test
    test

--- js/json.js
+++ js/json.js
@@ -10,9 +10,13 @@ $(document).ready(function(){
         
         $.each( json.projects, function( i, val ) {
             alert(val.length);
-            if (i === val.length - 1 || i === val.length - 2 || i === val.length - 3 || i === val.length -4) {
-                $("#project").html('<a class="header" href="#"><div class="card"><div class="image"><img src="https://tomeksdev.github.io/images/' + json.projects.project[i].img + '" /></div><div class="content"><p>' + json.projects.project[i].title + '</p><div class="meta"><span class="date">' + json.projects.project[i].date + '</span></div><div class="description"><p>' + json.projects.project[i].desc + '</p></div></div><div class="extra"><i class="fa ' + json.projects.project[i].statico + '"></i>' + json.projects.project[i].status + '</div></div></a>');
-            }
+            
+            $.each(json.projects.project, function(id, value){
+                if (id == val.length - 1 || id == val.length - 2 || id == val.length - 3 || id == val.length -4) {
+                    $("#project").html('<a class="header" href="#"><div class="card"><div class="image"><img src="https://tomeksdev.github.io/images/' + json.projects.project[id].img + '" /></div><div class="content"><p>' + json.projects.project[id].title + '</p><div class="meta"><span class="date">' + json.projects.project[id].date + '</span></div><div class="description"><p>' + json.projects.project[id].desc + '</p></div></div><div class="extra"><i class="fa ' + json.projects.project[id].statico + '"></i>' + json.projects.project[id].status + '</div></div></a>');
+                }    
+            });
+            
         });
     });
 });
