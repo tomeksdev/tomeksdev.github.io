efb83f1ab86819be7e0da2bb1788c239753dbded
Tomo Novak <tomeknomek@gmail.com>
2016-11-12 15:00:47 +0100

    Website v1.1.8
    Update project json and markdown. Show project write.

--- js/json.js
+++ js/json.js
@@ -6,5 +6,7 @@ $(document).ready(function(){
         $("#post-text").load("https://tomeksdev.github.io/post/src/" + json.posts.post[0].text, function(data) {
             $("#post-text").html(mmd(data));
         });
+        
+        $("#project").html('<a class="header" href="#"><div class="card"><div class="image"><img src="https://tomeksdev.github.io/images/' + json.projects.project[0].img + '" /></div><div class="content"><p>' + json.porjects.project[0].title + '</p><div class="meta"><span class="date">' + json.projects.project[0].date + '</span></div><div class="description"><p>' + json.projects.project[0].desc + '</p></div></div><div class="extra"><i class="fa ' + json.projects.project[0].statico + '"></i>' + json.projects.project[0].status + '</div></div></a>');
     });
 });
