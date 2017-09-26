5916921b728aa44de536945e9fe09d8aabcffa10
Tomo Novak <tomeknomek@gmail.com>
2017-09-26 13:55:54 +0200

    Website v2.0.1

---
+++ js/tomeksdev.js
@@ -0,0 +1,11 @@
+jQuery( document ).ready(function() {
+    
+    $(".nav-link").click(function(e) {
+        //console.log($(this).data('rel'));
+        $(".nav-link").removeClass("active");
+        $(this).addClass("active");
+        e.preventDefault();
+        $('.content-container div').hide('fade');
+        $('#' + $(this).data('rel')).show('slow');
+    });
+});
