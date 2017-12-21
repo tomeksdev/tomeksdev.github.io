d3d7427541e7cbaa57ab65a03e1a583fef4c624c
Tomo Novak <tomeknomek@gmail.com>
2017-12-21 08:28:34 +0100

    Update tomeksdev.js

--- js/tomeksdev.js
+++ js/tomeksdev.js
@@ -10,3 +10,22 @@ jQuery( document ).ready(function() {
         $('#mastfoot').show('slow');
     });
 });
+
+function loadJSON(callback) {   
+
+    var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'post/post.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);  
+ }
+
+loadJSON(function(response) {
+    var post = JSON.parse(response);
+     document.getElementById("demo").innerHTML = post.title + ", " + post.text;
+});
