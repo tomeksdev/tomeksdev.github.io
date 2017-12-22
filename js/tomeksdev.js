63d6868c24b90d9dbbcd174e342af08bec490424
Tomo Novak <tomeknomek@gmail.com>
2017-12-22 10:06:33 +0100

    Update tomeksdev.js

--- js/tomeksdev.js
+++ js/tomeksdev.js
@@ -26,20 +26,20 @@ jQuery( document ).ready(function() {
 		}
     });*/
 	$.ajax({
-   url: "https://tomeksdev.github.io/post/post.json",
-    type:"get",
-    dataType:'text',  
-    success: function(data){
-      console.log(data);
-    },
-    error:function() {
-      console.log("err");
-    }
-});
+	   url: "https://tomeksdev.github.io/post/post.json",
+	    type:"get",
+	    dataType:'text',  
+	    success: function(data){
+	      console.log(data);
+	    },
+	    error:function() {
+	      console.log("err");
+	    }
+	});
      
-    $.getJSON("https://tomeksdev.github.io/post/post.json", function(json){
+    /*$.getJSON("https://tomeksdev.github.io/post/post.json", function(json){
         alert("3");
-        /*$.each(json.posts, function(i,post){
+        $.each(json.posts, function(i,post){
             alert("3");
             //content = post.title;
             alert(post.title);
@@ -47,8 +47,8 @@ jQuery( document ).ready(function() {
             //content = post.text;
             $("#about .cover-heading").html(post.title);
             $("#about .lead").html(post.text);
-        });*/
+        });
          alert("4");
-    });  
+    });  */
      alert("5");
 });
