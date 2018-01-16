fd6879d178052783df79cf6dda5c6d5a924f0cba
Tomo Novak <tomeknomek@gmail.com>
2018-01-16 13:27:48 +0100

    Update tomeksdev.js

--- js/tomeksdev.js
+++ js/tomeksdev.js
@@ -27,39 +27,71 @@ $(document).ready(function() {
 	}
     });
 	
-    $("#target-editor-with-custom-buttons").markdown({
-  	additionalButtons: [
-    	[{
-              name: "groupCustom",
-              data: [{
-            	    name: "cmdBeer",
-            	    toggle: true, // this param only take effect if you load bootstrap.js
-                    title: "Beer",
-                    icon: "glyphicon glyphicon-glass",
-                    callback: function(e){
-		      // Replace selection with some drinks
-		      var chunk, cursor,
-			  selected = e.getSelection(), content = e.getContent(),
-			  drinks = ["Heinekken", "Budweiser",
-				    "Iron City", "Amstel Light",
-				    "Red Stripe", "Smithwicks",
-				    "Westvleteren", "Sierra Nevada",
-				    "Guinness", "Corona", "Calsberg"],
-			  index = Math.floor((Math.random()*10)+1)
+    // Most options demonstrate the non-default behavior
+	var simplemde = new SimpleMDE({
+		autofocus: true,
+		autosave: {
+			enabled: true,
+			uniqueId: "MyUniqueID",
+			delay: 1000,
+		},
+		blockStyles: {
+			bold: "__",
+			italic: "_"
+		},
+		element: document.getElementById("editor"),
+		forceSync: true,
+		hideIcons: ["guide", "heading"],
+		indentWithTabs: false,
+		initialValue: "Hello world!",
+		insertTexts: {
+			horizontalRule: ["", "\n\n-----\n\n"],
+			image: ["![](http://", ")"],
+			link: ["[", "](http://)"],
+			table: ["", "\n\n| Column 1 | Column 2 | Column 3 |\n| -------- | -------- | -------- |\n| Text     | Text      | Text     |\n\n"],
+		},
+		lineWrapping: false,
+		parsingConfig: {
+			allowAtxHeaderWithoutSpace: true,
+			strikethrough: false,
+			underscoresBreakWords: true,
+		},
+		placeholder: "Type here...",
+		previewRender: function(plainText) {
+			return customMarkdownParser(plainText); // Returns HTML from a custom parser
+		},
+		previewRender: function(plainText, preview) { // Async method
+			setTimeout(function(){
+				preview.innerHTML = customMarkdownParser(plainText);
+			}, 250);
 
-
-		      // Give random drink
-		      chunk = drinks[index]
-
-		      // transform selection and set the cursor into chunked text
-		      e.replaceSelection(chunk)
-		      cursor = selected.start
-
-		      // Set the cursor
-		      e.setSelection(cursor,cursor+chunk.length)
-		    }
-          	}]
-    	}]
-  	]
-    });
+			return "Loading...";
+		},
+		promptURLs: true,
+		renderingConfig: {
+			singleLineBreaks: false,
+			codeSyntaxHighlighting: true,
+		},
+		shortcuts: {
+			drawTable: "Cmd-Alt-T"
+		},
+		showIcons: ["code", "table"],
+		spellChecker: false,
+		status: false,
+		status: ["autosave", "lines", "words", "cursor"], // Optional usage
+		status: ["autosave", "lines", "words", "cursor", {
+			className: "keystrokes",
+			defaultValue: function(el) {
+				this.keystrokes = 0;
+				el.innerHTML = "0 Keystrokes";
+			},
+			onUpdate: function(el) {
+				el.innerHTML = ++this.keystrokes + " Keystrokes";
+			}
+		}], // Another optional usage, with a custom status bar item that counts keystrokes
+		styleSelectedText: false,
+		tabSize: 4,
+		toolbar: false,
+		toolbarTips: false,
+	});
 });
