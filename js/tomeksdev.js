7d6f47fa348b57b5c79dc3697aadc40fdf6184f6
Tomo Novak <tomeknomek@gmail.com>
2018-01-16 13:08:44 +0100

    Update tomeksdev.js

--- js/tomeksdev.js
+++ js/tomeksdev.js
@@ -26,4 +26,40 @@ $(document).ready(function() {
 	      console.log("err");
 	}
     });
+	
+    $("#target-editor-with-custom-buttons").markdown({
+  	additionalButtons: [
+    	[{
+              name: "groupCustom",
+              data: [{
+            	    name: "cmdBeer",
+            	    toggle: true, // this param only take effect if you load bootstrap.js
+                    title: "Beer",
+                    icon: "glyphicon glyphicon-glass",
+                    callback: function(e){
+		      // Replace selection with some drinks
+		      var chunk, cursor,
+			  selected = e.getSelection(), content = e.getContent(),
+			  drinks = ["Heinekken", "Budweiser",
+				    "Iron City", "Amstel Light",
+				    "Red Stripe", "Smithwicks",
+				    "Westvleteren", "Sierra Nevada",
+				    "Guinness", "Corona", "Calsberg"],
+			  index = Math.floor((Math.random()*10)+1)
+
+
+		      // Give random drink
+		      chunk = drinks[index]
+
+		      // transform selection and set the cursor into chunked text
+		      e.replaceSelection(chunk)
+		      cursor = selected.start
+
+		      // Set the cursor
+		      e.setSelection(cursor,cursor+chunk.length)
+		    }
+          	}]
+    	}]
+  	]
+    });
 });
