045f9b08201e9c0e8b6ca76b8f18080b06448947
Tomo Novak <tomeknomek@gmail.com>
2018-01-17 10:40:40 +0100

    Update tomeksdev.js

--- js/tomeksdev.js
+++ js/tomeksdev.js
@@ -18,8 +18,8 @@ $(document).ready(function() {
 	type:"get",
 	dataType:'json',  
 	success: function(data){
-	      var text = markdown.toHTML(data.posts.text);
-	      $(".blog .cover-heading").html(data.posts.title);
+	      var text = markdown.toHTML(data.posts[0].text);
+	      $(".blog .cover-heading").html(data.posts[0].title);
               $(".blog .lead").html(text);
 	},
 	error:function() {
