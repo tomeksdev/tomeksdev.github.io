8a63dc2a70e4836830786a3c0c112e4e2dd455c2
Tomo Novak <tomeknomek@gmail.com>
2018-01-17 11:03:30 +0100

    Update editorTDM.js

--- js/editorTDM.js
+++ js/editorTDM.js
@@ -1,2 +1,23 @@
 var editor = new Editor();
 editor.render();
+
+$(document).ready(function() {		
+    $.ajax({
+   	headers: {
+		'Access-Control-Allow-Origin': '*',
+	},
+	url: "http://tomeksdev.com/post/post.json",
+	type:"get",
+	dataType:'json',  
+	success: function(data){
+	      for(var i = 0; i < data.posts.length; i++)
+		      var j = i;
+		
+        var id = data.posts[j].id + 1;
+	      console.log(id);
+	},
+	error:function() {
+	      console.log("err");
+	}
+    });
+});
