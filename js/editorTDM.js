76a36c23cf7ef3b8ebacfa2a88120804141d82e5
Tomo Novak <tomeknomek@gmail.com>
2018-01-20 10:34:33 +0100

    Update editorTDM.js

--- js/editorTDM.js
+++ js/editorTDM.js
@@ -81,36 +81,29 @@ $(document).ready(function() {
 	    var data = e.params.data;
 	    console.log(data);
 	});*/
-	$("#post").select2({
-		tags: true,
-		multiple: true,
-		tokenSeparators: [',', ' '],
-		minimumInputLength: 2,
-		minimumResultsForSearch: 10,
+	   $("#e7").select2({
+		placeholder: "Search for a repository",
+		minimumInputLength: 3,
 		ajax: {
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-			},
-			url: "http://tomeksdev.com/post/post.json",
-			dataType: "json",
-			type: "GET",
-			data: function (params) {
+		    url: "https://api.github.com/search/repositories",
+		    dataType: 'json',
+		    quietMillis: 250,
+		    data: function (term, page) { // page is the one-based page number tracked by Select2
+			return {
+			    q: term, //search term
+			    page: page // page number
+			};
+		    },
+		    results: function (data, page) {
+			var more = (page * 30) < data.total_count; // whether or not there are more results available
 
-			    var queryParameters = {
-				term: params.term
-			    }
-			    return queryParameters;
-			},
-			processResults: function (data) {
-			    return {
-				results: $.map(data.posts, function (item) {
-				    return {
-					text: item.title,
-					id: item.id
-				    }
-				})
-			    };
-			}
-		}
-	});
+			// notice we return the value of more so Select2 knows if more results can be loaded
+			return { results: data.items, more: more };
+		    }
+		},
+		formatResult: repoFormatResult, // omitted for brevity, see the source of this page
+		formatSelection: repoFormatSelection, // omitted for brevity, see the source of this page
+		dropdownCssClass: "bigdrop", // apply css that makes the dropdown taller
+		escapeMarkup: function (m) { return m; } // we do not want to escape markup since we are displaying html in results
+	    });
 });
